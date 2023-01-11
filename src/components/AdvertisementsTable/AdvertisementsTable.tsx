70b017c79a8b69872c57cbeeaea86a84fe646d74
Victor Greco <33832977+VictorGreco@users.noreply.github.com>
2023-01-11 22:21:43 +0000

    feat: added data grid

--- src/components/AdvertisementsTable/AdvertisementsTable.tsx
+++ src/components/AdvertisementsTable/AdvertisementsTable.tsx
@@ -1,9 +1,20 @@
 import { useQuery, gql } from '@apollo/client';
 
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+
+interface AdvertisementsData {
+    _id: string;
+    title: string;
+    product_id: number,
+    valid_until: string;
+    discount_percentage: number;
+}
+
 const GET_ALL_ADVERTISEMENTS = gql`
   query GetAllAdvertisements {
     advertisements {
       _id
+      title
       product_id
       valid_until
       discount_percentage
@@ -13,13 +24,39 @@ const GET_ALL_ADVERTISEMENTS = gql`
 
 function AdvertisementsTable() {
     const { loading, error, data } = useQuery(GET_ALL_ADVERTISEMENTS);
+
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error : {error.message}</p>;
 
+    const columns = [
+        { field: '_id', headerName: 'Advertisement id', width: 250 },
+        { field: 'product_id', headerName: 'Product id', width: 250 },
+        { field: 'title', headerName: 'Title', width: 250 },
+        { field: 'valid_until', headerName: 'Valid until', width: 250 },
+        { field: 'discount_percentage', headerName: 'Discount percentage', width: 250 }
+    ]
+
   return (
-    <div className="App">
-        {JSON.stringify(data.advertisements)}
-    </div>
+    <section>
+        <div style={{ height: 400, width: '100%' }}>
+            <DataGrid
+                columns={columns}
+                rows={data.advertisements.map(({ _id, product_id, title, valid_until, discount_percentage }: AdvertisementsData, index: number) => {
+                    return {
+                        id: index,
+                        _id: _id,
+                        product_id,
+                        title,
+                        valid_until,
+                        discount_percentage
+                    }
+                })}
+                components={{
+                    Toolbar: GridToolbar,
+                }}
+            />
+        </div>
+    </section>
   );
 }
 
