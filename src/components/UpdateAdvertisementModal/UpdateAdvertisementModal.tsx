facbf23b9c1b09146ec266a72e82d6ad248f4859
Victor Greco <33832977+VictorGreco@users.noreply.github.com>
2023-01-12 18:01:08 +0000

    feat: sort table asc and load product id field

--- src/components/UpdateAdvertisementModal/UpdateAdvertisementModal.tsx
+++ src/components/UpdateAdvertisementModal/UpdateAdvertisementModal.tsx
@@ -156,6 +156,7 @@ export function UpdateAdvertisementModal(): JSX.Element {
                         select
                         required
                         id="outlined-required"
+                        defaultValue={productId}
                         onChange={handleProductIdChange}
                         label="Product id"
                     >
